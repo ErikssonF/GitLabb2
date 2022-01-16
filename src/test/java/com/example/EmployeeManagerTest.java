e0989bebd7f288501a7ffe5a407a97eb8d24398c
Fredrik Eriksson <eriksson.nf@gmail.com>
2022-01-16 14:42:25 +0100

    change project name in pom.xml file

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,16 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 class EmployeeManagerTest {
+
+    @Test
+    void test(){
+
+
+
+    }
 
 }
---
+++ src/test/java/com/example/bowlingGame/BowlingTest.java
@@ -0,0 +1,6 @@
+package com.example.bowlingGame;
+
+public class BowlingTest {
+
+
+}
